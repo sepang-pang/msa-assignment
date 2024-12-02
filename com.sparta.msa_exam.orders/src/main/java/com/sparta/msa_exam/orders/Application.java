b6b059e8897625fbed7d8a8d47c8ba0c760b8ba4
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-03 02:08:18 +0900

    [ feature ] : Order 기본 설정
    - `@EnableJpaAuditing` 및 `@EnableFeignClients` 어노테이션 추가
      - JPA Auditing 활성화
      - Feign Client 사용을 위한 설정 추가
    
    - DB 연결 설정 및 OSIV false 설정

--- com.sparta.msa_exam.orders/src/main/java/com/sparta/msa_exam/orders/Application.java
+++ com.sparta.msa_exam.orders/src/main/java/com/sparta/msa_exam/orders/Application.java
@@ -2,8 +2,12 @@ package com.sparta.msa_exam.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
