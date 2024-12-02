e7f732c57b2d05264d819abe36102f938143bf6f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-02 22:35:32 +0900

    [ feature ] : Product 기본 설정 및 Entity 설계
    - Open In View 설정 false로 비활성화
    
    - Product Entity 설계
      - `@Tsid` 적용하여 ID 생성 전략 사용
      - 생성/수정/삭제 관련 `Audit` 필드 추가
    
    - `@EnableJpaAuditing` 및 `@EnableFeignClients` 어노테이션 추가
      - JPA Auditing 활성화
      - Feign Client 사용을 위한 설정 추가

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/model/entity/ProductEntity.java
@@ -0,0 +1,64 @@
+package com.sparta.msa_exam.product.model.entity;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "products")
+public class ProductEntity {
+
+    @Id @Tsid
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "supply_price")
+    private int supplyPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at" , nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+
+    @Builder
+    public ProductEntity(String name, int supplyPrice, int quantity, String username) {
+        this.name = name;
+        this.supplyPrice = supplyPrice;
+        this.quantity = quantity;
+        this.createdBy = username;
+        this.modifiedBy = username;
+    }
+}
